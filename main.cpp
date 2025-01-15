90a09f330ef9e3741eebeae2c0cbebd1d2e34ef6
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2025-01-15 14:58:24 +0900

    01_05_確認課題

--- main.cpp
+++ main.cpp
@@ -175,11 +175,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	int mouseX, mouseY;
 	Novice::GetMousePosition(&mouseX, &mouseY);
 
-	Quaternion rotation = Quaternion(Vec3(1.0f, 0.4f, -0.2f).Normalized(), 0.45f);
-	Vec3 pointY = { 2.1f, -0.9f, 1.3f };
-	Mat4 rotateMatrix = Mat4::FromQuaternion(rotation);
-	Vec3 rotateByQuaternion = pointY.Rotate(rotation);
-	Vec3 rotateByMatrix = Mat4::Transform(pointY, rotateMatrix);
+	Quaternion rotation0 = Quaternion({ 0.71f, 0.71f, 0.0f }, 0.3f);
+	Quaternion rotation1 = Quaternion({ 0.71f, 0.0f, 0.71f }, 3.141592f);
+
+	Quaternion interpolate0 = Quaternion::Slerp(rotation0, rotation1, 0.0f);
+	Quaternion interpolate1 = Quaternion::Slerp(rotation0, rotation1, 0.3f);
+	Quaternion interpolate2 = Quaternion::Slerp(rotation0, rotation1, 0.5f);
+	Quaternion interpolate3 = Quaternion::Slerp(rotation0, rotation1, 0.7f);
+	Quaternion interpolate4 = Quaternion::Slerp(rotation0, rotation1, 1.0f);
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -302,10 +305,12 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 		ImGui::End();
 
-		QuaternionScreenPrintf(0, kRowHeight * 0, rotation, "		:rotation");
-		MatrixScreenPrintf(0, kRowHeight * 1, rotateMatrix, "rotateMatrix");
-		VectorScreenPrintf(0, kRowHeight * 6, rotateByQuaternion, "		:rotateByQuaternion");
-		VectorScreenPrintf(0, kRowHeight * 7, rotateByMatrix, "		:rotateByMatrix");
+		QuaternionScreenPrintf(0, kRowHeight * 0, interpolate0, "		:interpolate0, Slerp(q0, q1, 0.0f)");
+		QuaternionScreenPrintf(0, kRowHeight * 1, interpolate1, "		:interpolate1, Slerp(q0, q1, 0.3f)");
+		QuaternionScreenPrintf(0, kRowHeight * 2, interpolate2, "		:interpolate2, Slerp(q0, q1, 0.5f)");
+		QuaternionScreenPrintf(0, kRowHeight * 3, interpolate3, "		:interpolate3, Slerp(q0, q1, 0.7f)");
+		QuaternionScreenPrintf(0, kRowHeight * 4, interpolate4, "		:interpolate4, Slerp(q0, q1, 1.0f)");
+
 
 
 		///
